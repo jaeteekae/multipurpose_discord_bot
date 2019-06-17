95c44806011d3ff2576f9ba30ebd272b8e97eb55
jaeteekae <jaeteekae@gmail.com>
2019-06-16 23:00:51 -0400

    away and back commands work

--- bot.py
+++ bot.py
@@ -4,32 +4,118 @@ from discord.ext import commands
 import logging
 
 import random, json, os, pickle, time
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from apscheduler.schedulers.background import BackgroundScheduler
 import secret
 import pdb
-    # pdb.set_trace()
+# pdb.set_trace()
 
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = secret.TOKEN
-PREFIX = '!'
-DATA_FILE = './data.pkl'
-data = {'away': {}}
+PREFIX = "!"
+AWAY_FILE = "./away.json"
+data = {}
 
-bot = commands.Bot(command_prefix=PREFIX)
+bot = commands.Bot(command_prefix=PREFIX, case_insensitive=True)
 
+def init():
+    try:
+        with open(AWAY_FILE,'r') as f:
+            obj = json.load(f)
+            data["away"] = obj
+    except:
+        data["away"] = {}
+
+def write_to_disk():
+    with open(AWAY_FILE,'w') as f:
+        json.dump(data["away"],f)
+
+def get_emoji(guild, name):
+    emoji = discord.utils.get(guild.emojis, name=name)
+    return str(emoji)
+
+def generate_timestring(elapsed):
+    days, r = divmod(elapsed, 86400)
+    hours, r = divmod(r, 3600)
+    minutes, seconds = divmod(r, 60)
+
+    timestring = ""
+    if days:
+        timestring += "**{:.0f}** days ".format(days)
+    if hours:
+        timestring += "**{:.0f}** hours ".format(hours)
+    if minutes:
+        timestring += "**{:.0f}** minutes ".format(minutes)
+    if seconds:
+        timestring += "**{:.0f}** seconds".format(seconds)
+
+    return(timestring)
+
+def send_away_msg(mem):
+    obj = data["away"][str(mem.id)]
+    timestring = generate_timestring(time.time()-obj["time"])
+    response = "**{}** is away!\n**Duration:** {}\n**Message:** {}".format(mem.display_name,timestring,obj["message"])
+    return response
+
+############ OUT OF OFFICE ############
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def away(ctx, *args):
+    if not args:
+        response = '{}: plz supply a message so we know why you are away'.format(ctx.author.mention)
+        await ctx.send(response)
+        return
+    else:
+        args = " ".join(args)
 
-@bot.command()
-async def away(ctx, *,args):
+    emoji = get_emoji(ctx.guild, "jin_kiss")
     response = ''
+    memid = str(ctx.author.id)
     now = time.time()
     msg = str(args)
 
-    response = 'from: ' + ctx.author.mention + '\nmessage: ' + msg + '\ntime: ' + str(now)
-    await ctx.send(response)
-    print("achieved!")
+    obj = {"time": now, "message": msg}
+    data["away"][memid] = obj
 
+    response = 'Marking you away, {}, with the message *{}*\nWe will miss you {}'.format(ctx.author.mention, msg, emoji)
+    await ctx.send(response)
+
+@bot.command()
+async def back(ctx):
+    memid = str(ctx.author.id)
+
+    if memid not in data["away"]:
+        emoji = get_emoji(ctx.guild, "v_derp")
+        response = "You weren't away, {} {}".format(ctx.author.mention, emoji)
+        await ctx.send(response)
+        return
+    else:
+        obj = data["away"].pop(memid)
+        now = time.time()
+        # elapsed in seconds
+        elapsed = now - obj["time"]
+        timestring = generate_timestring(elapsed)
+
+        response = "Welcome back, {}!\nYou were away for: {}".format(ctx.author.mention, timestring)
+        await ctx.send(response)
+
+##### GENERAL MESSAGE HANDLING #####
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    for ment in message.mentions:
+        if str(ment.id) in data["away"]:
+            await message.channel.send(send_away_msg(ment))
+
+    await bot.process_commands(message)
+
+init()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(write_to_disk, 'interval', hours=1)
+scheduler.start()
 
 bot.run(TOKEN)
