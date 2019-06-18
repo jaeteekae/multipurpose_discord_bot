d29456dca26c3c669f657d2f9f52abf83ab84989
jaeteekae <jaeteekae@gmail.com>
2019-06-17 21:29:11 -0400

    birthday command works

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import logging
 
 import random, json, os, pickle, time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from bs4 import BeautifulSoup
 from apscheduler.schedulers.background import BackgroundScheduler
 import secret
@@ -16,24 +16,36 @@ logging.basicConfig(level=logging.INFO)
 TOKEN = secret.TOKEN
 PREFIX = "!"
 
-AWAY_FILE = "./away.json"
+AWAY_FILE = "data/away.json"
 AWAY_COLOR = 0x72FF7B
-data = {}
+
+BDAY_FILE = "data/bdays.json"
+BDAY_COLOR = 0x494ce5
 
 help_cmd = discord.ext.commands.DefaultHelpCommand(no_category="What can Apricot-Flower-Baby do for you")
 bot = commands.Bot(command_prefix=PREFIX, case_insensitive=True, help_command=help_cmd)
 
-def init():
-    try:
-        with open(AWAY_FILE,'r') as f:
+class DataObj:
+    away = {}
+    bdays = {}
+
+    def __init__(self):
+        try:
+            with open(AWAY_FILE,'r') as f:
+                obj = json.load(f)
+                self.away = obj
+        except:
+            pass
+
+        with open(BDAY_FILE,'r') as f:
             obj = json.load(f)
-            data["away"] = obj
-    except:
-        data["away"] = {}
+            self.bdays = obj
+
+data = DataObj()
 
 def write_to_disk():
     with open(AWAY_FILE,'w') as f:
-        json.dump(data["away"],f)
+        json.dump(data.away,f)
 
 def get_emoji(guild, name):
     emoji = discord.utils.get(guild.emojis, name=name)
@@ -75,7 +87,7 @@ class Out_of_Office(commands.Cog, name="Out of Office"):
         msg = str(args)
 
         obj = {"time": now, "message": msg}
-        data["away"][memid] = obj
+        data.away[memid] = obj
 
         response = 'Marking you away, {}, with the message *"{}"*\nWe will miss you {}'.format(ctx.author.mention, msg, emoji)
         await ctx.send(response)
@@ -84,13 +96,13 @@ class Out_of_Office(commands.Cog, name="Out of Office"):
     async def back(ctx):
         memid = str(ctx.author.id)
 
-        if memid not in data["away"]:
+        if memid not in data.away:
             emoji = get_emoji(ctx.guild, "v_derp")
             response = "You weren't away, {} {}".format(ctx.author.mention, emoji)
             await ctx.send(response)
             return
         else:
-            obj = data["away"].pop(memid)
+            obj = data.away.pop(memid)
             now = time.time()
             # elapsed in seconds
             elapsed = now - obj["time"]
@@ -100,7 +112,7 @@ class Out_of_Office(commands.Cog, name="Out of Office"):
             await ctx.send(response)
 
 def send_away_msg(mem):
-    obj = data["away"][str(mem.id)]
+    obj = data.away[str(mem.id)]
     elapsed = time.time()-obj["time"]
     timestring = generate_timestring(elapsed)
 
@@ -112,6 +124,46 @@ def send_away_msg(mem):
     responsetxt = "__Duration:__ {}\n__Since:__ {}\n__Message:__ {}".format(timestring,since,obj["message"])
     return(titletxt, responsetxt)
 
+######## BIRTHDAYS ########
+@bot.command()
+async def birthday(ctx):
+    today = datetime.today()
+    this_year = today.year
+    this_month = today.month
+    this_day = today.day
+    sumthisday = 31*this_month+this_day
+    closest_bday = [12,31,365]
+
+    for person in data.bdays:
+        dob = person['dob'].split('/')
+        mon = int(dob[0])
+        day = int(dob[1])
+        sumdays = 31*mon+day
+        diff = sumdays-sumthisday
+
+        # is a contender
+        if diff>0 and diff<closest_bday[2]:
+            closest_bday[0] = mon
+            closest_bday[1] = day
+            closest_bday[2] = diff
+
+    for person in data.bdays:
+        dob = person['dob'].split('/')
+        mon = int(dob[0])
+        day = int(dob[1])
+        if (mon*31+day)-sumthisday>0:
+            yr = this_year
+        else:
+            yr = this_year+1
+        datestr = date(yr,mon,day).strftime('On %A, %B %d')
+
+        if mon==closest_bday[0] and day==closest_bday[1]:
+
+            msg = "🎊 The next birthday is " + person['name'] + "'s! 🎊\n" + datestr
+            emb = discord.Embed(description=msg,color=BDAY_COLOR)
+            await ctx.send(embed=emb)
+
+
 ##### GENERAL MESSAGE HANDLING #####
 @bot.event
 async def on_message(message):
@@ -119,14 +171,13 @@ async def on_message(message):
         return
 
     for ment in message.mentions:
-        if str(ment.id) in data["away"]:
+        if str(ment.id) in data.away:
             title,desc = send_away_msg(ment)
             emb = discord.Embed(title=title,description=desc,color=AWAY_COLOR)
             await message.channel.send(embed=emb)
 
     await bot.process_commands(message)
 
-init()
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(write_to_disk, 'interval', hours=1)
