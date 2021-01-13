7566e9f4561ba59e2dae38c6777474e086af5f45
jaeteekae <jaeteekae@gmail.com>
2021-01-13 18:53:51 -0500

    fixing up the scraper

--- ao3_scraper.py
+++ ao3_scraper.py
@@ -19,7 +19,7 @@ def ao3_embed(msg):
 		emb.set_author(name=author.string, url=auth_url, icon_url='https://archiveofourown.org/images/ao3_logos/logo_42.png')
 	except:
 		# if the fic is locked
-		emb.set_author(name="Locked fic", url=url, icon_url='https://archiveofourown.org/images/ao3_logos/logo_42.png')
+		emb.set_author(name="Locked fic 🔒", url=url, icon_url='https://archiveofourown.org/images/ao3_logos/logo_42.png')
 		return emb
 
 	# fic title
@@ -27,49 +27,62 @@ def ao3_embed(msg):
 	emb.title = title.string
 	emb.url = url
 
+	# fandoms
+	fandoms = soup.find("dd", {"class": "fandom tags"}).findAll("a")
+	fanstr = ""
+	for f in fandoms:
+		fanstr += "[{}]({}), ".format(f.string, "https://archiveofourown.org"+f['href'])
+	emb.add_field(name="__Fandom__", value=fanstr[:-2], inline=True)
+
 	# rating
 	rating = soup.find("dd", {"class": "rating tags"})
-	emb.add_field(name="Rating", value=rating.get_text(), inline=True)
+	emb.add_field(name="__Rating__", value=rating.get_text(), inline=True)
 
 	# archive warnings
 	warnings = soup.find("dd", {"class": "warning tags"}).findAll("a")
 	warnstr = ""
 	for w in warnings:
 		warnstr += "{}, ".format(w.string)
-	emb.add_field(name="Archive Warnings", value=warnstr[:-2], inline=True)
-
-	# chapters
-	chapters = soup.find("dd", {"class": "chapters"})
-	emb.add_field(name="Chapters", value=chapters.string, inline=True)
+	emb.add_field(name="__Archive Warnings__", value=warnstr[:-2], inline=True)
 
 	# words
 	words = soup.find("dd", {"class": "words"})
-	emb.add_field(name="Words", value=words.string, inline=False)
+	emb.add_field(name="__Words__", value=words.string, inline=True)
+
+	# chapters
+	chapters = soup.find("dd", {"class": "chapters"})
+	emb.add_field(name="__Chapters__", value=chapters.string, inline=True)
 
 	# pairings
-	pairings = soup.find("dd", {"class": "relationship tags"}).findAll("a")
-	relstr = ""
-	for p in pairings:
-		relstr += "[{}]({}), ".format(p.string, "https://archiveofourown.org"+p['href'])
-	emb.add_field(name="Relationships", value=relstr[:-2], inline=False)
+	try:
+		pairings = soup.find("dd", {"class": "relationship tags"}).findAll("a")
+		relstr = ""
+		for p in pairings:
+			relstr += "[{}]({}), ".format(p.string, "https://archiveofourown.org"+p['href'])
+	except:
+		relstr = "None, "
+	emb.add_field(name="__Relationships__", value=relstr[:-2], inline=False)
 
 	# tags
-	tags = soup.find("dd", {"class": "freeform tags"}).findAll("a")
-	tagstr = ""
-	for i in range(len(tags)):
-		t = tags[i]
-		if len(tagstr) + len(t.string) > 1000:
-			tagstr += "+{}, ".format(len(tags)-i)
-			break
-		tagstr += "{}, ".format(t.string)
-	emb.add_field(name="Tags", value=tagstr[:-2], inline=False)
+	try:
+		tags = soup.find("dd", {"class": "freeform tags"}).findAll("a")
+		tagstr = ""
+		for i in range(len(tags)):
+			t = tags[i]
+			if len(tagstr) + len(t.string) > 1000:
+				tagstr += "+{}, ".format(len(tags)-i)
+				break
+			tagstr += "{}, ".format(t.string)
+	except:
+		tagstr = "None, "
+	emb.add_field(name="__Tags__", value=tagstr[:-2], inline=False)
 
 	# summary
 	summary = soup.find("blockquote", {"class": "userstuff"}).get_text()
 	if len(summary) < 1010:
-		emb.add_field(name="Summary", value=summary, inline=False)
+		emb.add_field(name="__Summary__", value=summary, inline=False)
 	else:
-		emb.add_field(name="Summary", value=summary[:1010]+"...", inline=False)
+		emb.add_field(name="__Summary__", value=summary[:1010]+"...", inline=False)
 
 	return emb
 
